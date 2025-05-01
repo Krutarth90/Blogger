db3467a9ac53709126e272e7822b7ac92db2e87d
krutarth <krutarthpipaliya90@gmail.com>
2025-05-01 16:31:14 +0530

    finited

---
+++ Frontend/src/pages/signup.tsx
@@ -0,0 +1,10 @@
+import { Heading } from "../components/Heading";
+
+export function Signup (){
+
+    return (
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <Heading text = "HI THERE"/>
+        </div>
+    )
+}
