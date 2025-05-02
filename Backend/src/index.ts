445f241fe402aeee603292a5f3264a9b346ce25e
krutarth <krutarthpipaliya90@gmail.com>
2025-05-02 14:19:10 +0530

    fixed BE

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -15,64 +15,68 @@ app.post('/api/v1/signin', prismaMake, signIn);
 
 app.post('/api/v1/blog', auth, prismaMake, async (c)=>{
     const body = await c.req.json();
-    const {success, error} = postSchema.safeParse(body);
+    const authId = c.get('userId');
+    body.authId = authId;
+    const {success, error} =  postSchema.safeParse(body);
     if(!success)
         return c.json({
             Error : error.format()
         }, 400);
     const prisma = c.get('prisma');
-    const authId = c.get('userId');
-    body.authId = authId;
+    
     try 
     {
         const post = await prisma.post.create({
             data : body
         });
-        c.json({
+        return c.json({
             id : post.id
         });
     }
     catch(e)
     {
-        c.json({
+        console.log(e);
+        return c.json({
             msg : " ERROR "
         });
-        console.log(e);
+        
     }
 });
 
 app.put('/api/v1/blog', auth, prismaMake, async (c)=>{
     const body = await c.req.json();
+    const prisma = c.get('prisma');
+    const authId = c.get('userId');
+    
     const {success, error} = updatePost.safeParse(body);
     if(!success)
         return c.json({
             Error : error.format()
         }, 400)
-    const prisma = c.get('prisma');
-    const authId = c.get('userId');
     body.authId = authId;
     try 
     {
         const post = await prisma.post.update({
             where : {
                 id : body.id,
-                authId
+                authId : body.authId
             },
             data : {
                 title : body.title,
                 content : body.content
             }
         });
-        c.json({
+        return c.json({
             id : post.id
         });
     }
     catch(e)
     {
-        c.json({
+        console.log(e);
+        return c.json({
             msg : " ERROR "
         });
-        console.log(e);
+        
     }
 });
 
@@ -96,16 +100,17 @@ app.get('/api/v1/blog/:id', auth, prismaMake, async (c)=>{
     }
 });
 
-app.get('/api/v1/blog/bulk', prismaMake, async (c)=>{
+app.get('/api/v1/bulk', prismaMake, async (c)=>{
     const prisma = c.get('prisma');
     try {
         const posts = await prisma.post.findMany();
-        c.json({
+        console.log(posts);
+        return c.json({
             posts
         });
     } catch (e) {
         console.log(e);
-        c.json({
+        return c.json({
             message : " ERROR "
         });
     } 
